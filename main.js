3f7e5e9c6690ad4876b1d014eab196059a2e1f39
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2024-11-15 18:52:24 +0330

    Fix toDoContainer in main.js

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 let addToDoButton = document.getElementById("addToDo");
-let todoContainer = document.getElementById("toDoContainer");
+let toDoContainer = document.getElementById("toDoContainer");
 let inputField = document.getElementById("inputField");
 
 addToDoButton.addEventListener("click", function () {
   let paragraph = document.createElement("p");
   paragraph.classList.add("paragraph-styling");
   paragraph.innerText = inputField.value;
-  todoContainer.appendChild(paragraph);
+  toDoContainer.appendChild(paragraph);
   inputField.value = "";
   paragraph.addEventListener("click", function () {
     paragraph.style.textDecoration = "line-through";
   });
   paragraph.addEventListener("dblclick", function () {
-    todoContainer.removeChild(paragraph);
+    toDoContainer.removeChild(paragraph);
   });
 });
